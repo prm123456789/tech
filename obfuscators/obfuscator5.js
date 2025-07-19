60692e05b167092d08f4cb0953454283637f596f
prm123456789 <guerrierjojo8@gmail.com>
2025-07-19 19:03:47 -0300

    Update obfuscator5.js

--- obfuscators/obfuscator5.js
+++ obfuscators/obfuscator5.js
@@ -1,1 +1,16 @@
+export default function obfuscator5(code) {
+  const parts = code.match(/.{1,10}/g); // coupe par tranches de 10
+  const joined = parts.map(p => `"${p}"`).join(' +\n    ');
 
+  return `
+/**
+ * Obfusqué par INCONNU BOY OFFUSCATOR
+ * Méthode : Fragmentation du code
+ */
+(function(){
+  const code = 
+    ${joined};
+  eval(code);
+})();
+`;
+}
