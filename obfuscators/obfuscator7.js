b3e694479c532774fcebf6826260f753b53ef78c
prm123456789 <guerrierjojo8@gmail.com>
2025-07-19 19:04:46 -0300

    Update obfuscator7.js

--- obfuscators/obfuscator7.js
+++ obfuscators/obfuscator7.js
@@ -1,1 +1,23 @@
+export default function obfuscator7(code) {
+  const fakeCode = `
+let _x1 = 123456;
+function fakeFunc() {
+  return _x1 * Math.random();
+}
+`;
 
+  const wrapped = `
+(function(){
+  ${fakeCode}
+  eval(\`${code.replace(/`/g, '\\`')}\`);
+})();
+`;
+
+  return `
+/**
+ * Obfusqué par INCONNU BOY OFFUSCATOR
+ * Méthode : Injection de leurres
+ */
+${wrapped}
+`;
+}
