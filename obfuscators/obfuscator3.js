169e28983e9a6c1d7efc6f1d4a5aee89b4633e2a
prm123456789 <guerrierjojo8@gmail.com>
2025-07-19 19:02:32 -0300

    Update obfuscator3.js

--- obfuscators/obfuscator3.js
+++ obfuscators/obfuscator3.js
@@ -1,1 +1,14 @@
-
+export default function obfuscator3(code) {
+  const reversed = code.split('').reverse().join('');
+  return `
+/**
+ * Obfusqué par INCONNU BOY OFFUSCATOR
+ * Méthode : Inversion de code
+ */
+(function(){
+  const reversed = '${reversed}';
+  const normal = reversed.split('').reverse().join('');
+  eval(normal);
+})();
+`;
+}
