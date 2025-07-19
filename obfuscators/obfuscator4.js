e56a5fbd6aa92e5ea73bd5ed894b686b3b1a18d6
prm123456789 <guerrierjojo8@gmail.com>
2025-07-19 19:03:13 -0300

    Update obfuscator4.js

--- obfuscators/obfuscator4.js
+++ obfuscators/obfuscator4.js
@@ -1,1 +1,13 @@
+export default function obfuscator4(code) {
+  const unicode = code.split('')
+    .map(c => '\\u' + c.charCodeAt(0).toString(16).padStart(4, '0'))
+    .join('');
 
+  return `
+/**
+ * Obfusqué par INCONNU BOY OFFUSCATOR
+ * Méthode : Encodage Unicode
+ */
+eval("${unicode}");
+`;
+}
