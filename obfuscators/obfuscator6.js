3912318dfd46f6c71bb0d92bf3d8c4d939e96280
prm123456789 <guerrierjojo8@gmail.com>
2025-07-19 19:04:27 -0300

    Update obfuscator6.js

--- obfuscators/obfuscator6.js
+++ obfuscators/obfuscator6.js
@@ -1,1 +1,16 @@
+export default function obfuscator6(code) {
+  const minified = code
+    .replace(/\/\/.*$/gm, '')     // commentaires simples
+    .replace(/\/\*[\s\S]*?\*\//g, '') // commentaires multi-ligne
+    .replace(/\s+/g, ' ')         // espaces multiples
+    .replace(/\n/g, '')           // retours à la ligne
+    .trim();
 
+  return `
+/**
+ * Obfusqué par INCONNU BOY OFFUSCATOR
+ * Méthode : Minification simple
+ */
+eval("${minified}");
+`;
+}
