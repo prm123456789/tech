66f1868862bb8abcdc07eab19b93b9b03689229b
prm123456789 <guerrierjojo8@gmail.com>
2025-07-19 19:02:07 -0300

    Update obfuscator2.js

--- obfuscators/obfuscator2.js
+++ obfuscators/obfuscator2.js
@@ -1,1 +1,21 @@
+export default function obfuscator2(code) {
+  let encrypted = '';
+  for (let i = 0; i < code.length; i++) {
+    encrypted += String.fromCharCode(code.charCodeAt(i) + 3);
+  }
 
+  return `
+/**
+ * Obfusqué par INCONNU BOY OFFUSCATOR
+ * Méthode : Décalage ASCII +3
+ */
+(function(){
+  let decoded = '';
+  const data = '${encrypted}';
+  for (let i = 0; i < data.length; i++) {
+    decoded += String.fromCharCode(data.charCodeAt(i) - 3);
+  }
+  eval(decoded);
+})();
+`;
+}
